f57c9804c193b763d2eee72a33cc5100045c13ac
ponte <ponte@CAP-CEZARY>
2018-01-09 11:09:46 +0100

    [Eureka] Init

--- social-multiplication/src/main/java/microservices/com/socialmultiplication/SocialMultiplicationApplication.java
+++ social-multiplication/src/main/java/microservices/com/socialmultiplication/SocialMultiplicationApplication.java
@@ -2,7 +2,10 @@ package microservices.com.socialmultiplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class SocialMultiplicationApplication {
 
