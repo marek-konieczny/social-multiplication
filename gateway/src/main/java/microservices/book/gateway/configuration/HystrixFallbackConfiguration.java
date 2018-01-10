0cbc7943d4acf8f0892179c653abf6a8638520f4
ponte <ponte@CAP-CEZARY>
2018-01-10 15:59:56 +0100

    [Fix] Config fix

--- gateway/src/main/java/microservices/book/gateway/configuration/HystrixFallbackConfiguration.java
+++ gateway/src/main/java/microservices/book/gateway/configuration/HystrixFallbackConfiguration.java
@@ -1,9 +1,10 @@
 package microservices.book.gateway.configuration;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -11,58 +12,64 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
 
-/**
- * @author moises.macero
- */
 @Configuration
 public class HystrixFallbackConfiguration {
 
-    @Bean
-    public ZuulFallbackProvider zuulFallbackProvider() {
-        return new ZuulFallbackProvider() {
+	@Bean
+	public FallbackProvider fallbackProvider() {
+		return new FallbackProvider() {
 
-            @Override
-            public String getRoute() {
-                // Might be confusing: it's the serviceId property and not the route
-                return "multiplication";
-            }
+			@Override
+			public String getRoute() {
+				// Might be confusing: it's the serviceId property and not the route
+				return "multiplication";
+			}
 
-            @Override
-            public ClientHttpResponse fallbackResponse() {
-                return new ClientHttpResponse() {
-                    @Override
-                    public HttpStatus getStatusCode() throws IOException {
-                        return HttpStatus.OK;
-                    }
+			@Override
+			public ClientHttpResponse fallbackResponse() {
+				return new ClientHttpResponse() {
+					@Override
+					public HttpStatus getStatusCode() throws IOException {
+						return HttpStatus.OK;
+					}
 
-                    @Override
-                    public int getRawStatusCode() throws IOException {
-                        return HttpStatus.OK.value();
-                    }
+					@Override
+					public int getRawStatusCode() throws IOException {
+						return HttpStatus.OK.value();
+					}
 
-                    @Override
-                    public String getStatusText() throws IOException {
-                        return HttpStatus.OK.toString();
-                    }
+					@Override
+					public String getStatusText() throws IOException {
+						return HttpStatus.OK.toString();
+					}
 
-                    @Override
-                    public void close() {}
+					@Override
+					public void close() {
+					}
 
-                    @Override
-                    public InputStream getBody() throws IOException {
-                        return new ByteArrayInputStream("{\"factorA\":\"Sorry, Service is Down!\",\"factorB\":\"?\",\"id\":null}".getBytes());
-                    }
+					@Override
+					public InputStream getBody() throws IOException {
+						return new ByteArrayInputStream(
+								"{\"factorA\":\"Sorry, Service is Down!\",\"factorB\":\"?\",\"id\":null}".getBytes());
+					}
 
-                    @Override
-                    public HttpHeaders getHeaders() {
-                        HttpHeaders headers = new HttpHeaders();
-                        headers.setContentType(MediaType.APPLICATION_JSON);
-                        headers.setAccessControlAllowCredentials(true);
-                        headers.setAccessControlAllowOrigin("*");
-                        return headers;
-                    }
-                };
-            }
-        };
-    }
+					@Override
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						headers.setAccessControlAllowCredentials(true);
+						headers.setAccessControlAllowOrigin("*");
+						return headers;
+					}
+				};
+			}
+
+			@Override
+			public ClientHttpResponse fallbackResponse(Throwable cause) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+
+	}
 }
